d14479db56b6a2c1adf2d66d558c30816701e3ab
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-05-02 22:05:54 -0500

    retrieve ingress list from k8s api

---
+++ ingress.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python
+
+from kubernetes import client, config
+import json
+import time
+import os
+import ping
+
+def getIngress():
+    kubeConfig = '/tmp/config'
+    ingressList = []
+    ingress = {}
+    global config
+
+    if os.path.exists(kubeConfig):
+        config.load_kube_config(kubeConfig)
+        print '\n Using local Kube config for API authentication/authorization...\n'
+    else:
+        tokenFile = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+        caFile = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+        with open (tokenFile, "r") as apiToken:
+            token = apiToken.read()
+        apiToken.close
+        config = client.Configuration()
+        config.host = "https://kubernetes.default.svc.cluster.local"
+        config.api_key = {"authorization": "Bearer " + token}
+        config.ssl_ca_cert = caFile
+        client.Configuration.set_default(config)
+        print '\n Using cluster service account token for API authentication/authorization...\n'
+
+    v1beta1 = client.ExtensionsV1beta1Api()
+
+    response = json.loads(v1beta1.list_ingress_for_all_namespaces(_preload_content=False))
+    for item in response['items']:
+        ingress['name'] = item['metadata']['name']
+        ingress['namespace'] = item['metadata']['namespace']
+        for rule in item['spec']['rules']:
+            ingress['host'] = rule['host']
+            for path in rule['http']['paths']:
+                ingress['serviceName'] = path['backend']['serviceName']
+        ingressList.append(ingress)
+    return ingressList
+
+if __name__ == "__main__":
+    getIngress()
---
+++ test/test_ingress.py
@@ -0,0 +1,45 @@
+import pytest
+import os
+import sys
+import json
+import mock
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import ingress
+
+ingressSample = {
+  "items": [
+    {
+      "metadata": {
+        "name": "testy",
+        "namespace": "test"
+      },
+      "spec": {
+        "rules": [
+          {
+            "host": "test.io",
+            "http": {
+              "paths": [
+                {
+                  "backend": {
+                    "serviceName": "testem",
+                    "servicePort": 80
+                  },
+                  "path": "/"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+
+@mock.patch('kubernetes.client.ExtensionsV1beta1Api.list_ingress_for_all_namespaces')
+def test_get_ingress_objects(mock_list_ingress_func):
+    mock_list_ingress_func.return_value = json.dumps(ingressSample)
+    ingressList = ingress.getIngress()
+    assert ingressList[0]['name'] == 'testy'
+    assert ingressList[0]['namespace'] == 'test'
+    assert ingressList[0]['serviceName'] == 'testem'
+    assert ingressList[0]['host'] == 'test.io'
