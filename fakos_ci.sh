508a83d7042945e8e9214376c9c7cae3dde3895f
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-05-15 13:56:49 -0500

    run unit tests w/o docker image

--- fakos_ci.sh
+++ fakos_ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --rm -i -v $SCRIPT_DIR:/tmp -w /tmp/docker/test christiantragesser/dind-ci pytest
+pytest $SCRIPT_DIR/docker/test
