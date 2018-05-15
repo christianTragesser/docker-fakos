5e812c6995abf6c04baa05246e0fbb5231625fff
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-05-15 13:50:45 -0500

    remove tty requirement for testing

--- fakos_ci.sh
+++ fakos_ci.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --rm -it -v $SCRIPT_DIR:/tmp -w /tmp/docker/test christiantragesser/dind-ci pytest
+docker run --rm -i -v $SCRIPT_DIR:/tmp -w /tmp/docker/test christiantragesser/dind-ci pytest
