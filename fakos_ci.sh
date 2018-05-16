a21bc82bf27fa3aa3b061ddb3d37416e64e74b53
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-05-15 19:43:18 -0500

    deploy to dockerhub

--- fakos_ci.sh
+++ fakos_ci.sh
@@ -2,6 +2,38 @@
 
 set -e
 
+SCRIPT=`basename "$0"`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $SCRIPT_DIR/docker/test && pytest
+if [ ! $# == 1 ]; then
+    printf "\n $SCRIPT script needs a parameter: unit, test, etc. \n ex: '$SCRIPT test' \n\n"
+    exit 1
+fi
+
+OPTION=$1
+NETWORK="ci_net"
+CYAN='\033[1;36m'
+PURPLE='\033[1;35m'
+NC='\033[0m'
+
+function ci {
+  case $OPTION in
+    unit) unit
+    ;;
+    test) unit
+          build
+    ;;
+    *) printf "\n *** Selection $OPTION not found, exiting. ***\n\n"
+          exit 1
+  esac
+}
+
+function unit {
+  docker run --rm -i -v $SCRIPT_DIR:/tmp -w /tmp/docker/test christiantragesser/dind-ci pytest
+}
+
+function build {
+  cd $SCRIPT_DIR/docker && docker build -t christiantragesser/fakos .
+}
+
+ci
