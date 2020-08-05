351a7a3acc310b9bbe9e553151a0e3abf450df57
Christian Tragesser <christian.tragesser@wwt.com>
2020-08-05 09:18:35 -0500

    flake8 linting

--- ci.py
+++ ci.py
@@ -3,9 +3,10 @@ from argparse import ArgumentParser
 from pyplineCI import Pipeline
 
 dirPath = os.path.dirname(os.path.realpath(__file__))
-buildPath = dirPath+'/src/'
+buildPath = dirPath + '/src/'
 pipeline = Pipeline(dockerRegistry='registry.gitlab.com/christiantragesser')
 localTag = 'local/fakos'
+
 
 def ci(option):
     stage = {
@@ -16,29 +17,32 @@ def ci(option):
     run = stage.get(option, test)
     run()
 
+
 def test():
     testDir = '/tmp/docker/test'
     volumes = {
-        dirPath: { 'bind': '/tmp', 'mode': 'rw'}
+        dirPath: {'bind': '/tmp', 'mode': 'rw'}
     }
     print('Starting tests:')
-    pipeline.runi(image=pipeline.dockerRegistry+'/pypline-ci:fakos',
-                                     name='fakos-test', working_dir=testDir, volumes=volumes, command='pytest')
+    pipeline.runi(image=pipeline.dockerRegistry + '/pypline-ci:fakos',
+                  name='fakos-test', working_dir=testDir, volumes=volumes, command='pytest')
     pipeline.build_image(buildPath, localTag)
     print('Testing complete')
+
 
 def securityScan():
     print('Starting security scans:')
     pipeline.cve_scan(localTag)
 
+
 def local():
     volumes = {
-        dirPath: { 'bind': '/tmp', 'mode': 'rw'}
+        dirPath: {'bind': '/tmp', 'mode': 'rw'}
     }
     print('Initializing locally built instance:')
-    pipeline.build_image(buildPath,localTag)
-    pipeline.runi(image=localTag,
-                                     name='fakos-local', working_dir='/tmp', volumes=volumes, command='/bin/sh')
+    pipeline.build_image(buildPath, localTag)
+    pipeline.runi(image=localTag, name='fakos-local', working_dir='/tmp', volumes=volumes, command='/bin/sh')
+
 
 def main():
     parser = ArgumentParser(prog='ci-py')
@@ -46,6 +50,7 @@ def main():
                         help='run pipeline stage; test, scan, local')
     args = parser.parse_args()
     ci(args.stage)
+
 
 if __name__ == '__main__':
     main()
